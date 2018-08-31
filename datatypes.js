c57cb9c5fb837dbe93e6b7f059aab9d25335f79c
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-08-30 21:41:15 -0400

    Updates to datatypes

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+/*
+let x = 7; //This does not have a data type yet...only the values that we store in the variables have it
+console.log(typeof x);
+
+let y = true;
+console.log(typeof y);
+
+let z = "Jimmy";
+console.log(typeof z);
+*/
+
+let a;
+console.log(a);
+console.log(typeof a);
