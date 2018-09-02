61225cd88b8c0ecc51be69ed29a7aeea00977510
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-02 11:58:59 -0400

    Operators explained

---
+++ expressions.js
@@ -0,0 +1,24 @@
+//This is bad:
+//a;
+
+
+//Types of expressions
+//===========================
+//1. Variable Declaration
+
+//Assign a value
+//a=4;
+
+//Perform an evaluation that returns a single value
+//b + c
+
+
+let b =3;
+let c = 2;
+
+//There are actually exactly 3 different expressions in this single statement
+let = a = b + c;
+
+//1. Let a .... (A variable declaration)
+//2. Performing an evaluation of b + c using the addition operator
+//3. The result of b + c is assinged to a
---
+++ operators.js
@@ -0,0 +1,64 @@
+//Categories of Operators
+
+/*1. Assignment: "="
+ a = 10;
+*/
+/*=============================================================================
+
+2. Arithmetic: "+", "-", "*", "/", "%" (Remainder Amt...called "Modulus")
+
+var m = 10 % 3;
+console.log(m);
+*/
+
+/*=============================================================================
+3. Increment/Decrement: "++", "--"
+var a = 1;
+a++;
+console.log(a++); //This increment operator works after the 'a' has already been
+printed to the screen so it still appears as 2 until we reprint it on the next line
+We could have used a ++a to then display 3 in the first console.log instead
+console.log(a);
+*/
+
+/*=============================================================================
+4. String: "+" (As concatenation, NOT ADDITION)
+
+*/
+
+/*=============================================================================
+5. Precedence: "()" (You can use parens to specify order of ops)
+var b = 1 + 2 * 3; //This is a completely different result than the line that follows
+var c = (1 + 2) * 3; //Order of Ops PEMADS
+console.log(b);
+console.log(c);
+
+*/
+/*=============================================================================
+6. Function Invocation Operators: "()" (You can also use parens to invocate a function)
+console.log('Hello'); The parens are fucntion invocation ops...This is showing us
+a function called "log"..You can call it with arguments..We are passing in a
+string argument "Hello"
+*/
+
+/*=============================================================================
+7. Logical Operators: "||" (or), "&&" (and)
+
+*/
+/*=============================================================================
+8. Member Accessor Operator: //These is the "." in Console.Log()...It allows
+us to access the different "members" of this specific object
+
+this allows us to access the log function of the console object inside of JavaScript
+
+*/
+
+/*=============================================================================
+9. Code Block Operators "{}"
+
+*/
+
+/*=============================================================================
+10. Array Element Access Operator "[]"
+
+*/
