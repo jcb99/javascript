cc4fd6cf806ce86f58d591a81570fe59fe746fd7
jcb99 <jacob.bloom1999@gmail.com>
2018-09-17 08:47:17 -0400

    commit

--- tipcalculator.js
+++ tipcalculator.js
@@ -4,22 +4,23 @@ document.getElementById("totalTip").style.display="none"; //This hides the Tip A
 document.getElementById("calculate").onclick = function() {calculateTip()};
 
 
-//Check for zero or null values
-if (peopleAmt <= 0) {
-  alert("You must enter a valid number of people! It must be greater than 0.");
 
-}
 
 function calculateTip() {
   let billAmt = document.getElementById("billamt").value;
   let serviceQual = document.getElementById("serviceQual").value; //Tells how
   let peopleAmt = document.getElementById("peopleamt").value; //To know the number of people to split the tip by
 
+  //Check for zero or null values
+ if (peopleAmt <= 0 || isNaN(peopleAmt)) {
+    alert("You must enter a valid number of people! It must be greater than 0.");
+    return;
+  }
 
-
-
-
-
+  if (billAmt< 0 || isNaN(billAmt)){
+    alert("You must enter a bill amount that is greater than 0!");
+    return;
+  }
 
 
   //calc total
