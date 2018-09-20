5f0a90a4ce784b80c3b1fbadcb1f24a27295d185
jcb99 <42805997+jcb99@users.noreply.github.com>
2018-09-19 22:57:37 -0400

    commit

---
+++ todolist.js
@@ -0,0 +1,16 @@
+let x = document.getElementsByTagName("button");
+x[0].onclick = function() {addLi()};
+
+function addLi() {
+    let ul = document.getElementById('incomplete-tasks');
+    let li = document.createElement('li');
+    let numChildren = ul.children.length +1;
+    let aName = document.getElementById("new-task").value.trim();
+
+    if (aName != "") {
+      li.setAttribute("id", "element" + numChildren);
+      li.appendChild(document.createTextNode(aName));
+      ul.appendChild(li);
+      document.getElementById('new-task').value = '';
+    }
+  }
